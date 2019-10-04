61295e31b6aa8dfbf7992a3b8838a6464239bd96
Impressivenerd <jeff@patyk.dev>
2019-10-04 15:28:34 -0400

    Add Params, Batt Status & Reformat Console Output
    Because:
     - Param "ReadOnly" Closes #2
     - Param "PollingFrequency" allows configurable recording accuracy
     - Param "Output" allows path definition without prompt
     - Format-Table makes for a prettier console output
     - Battery Status gives an indication to laptop state at point-in-time

--- battery_benchmark.ps1
+++ battery_benchmark.ps1
@@ -6,36 +6,83 @@
 # License: MIT
 ####
 
-$path = Read-Host -Prompt "Input a filename"
+param (
+    # Write details only to the console. Do not write anything to a file
+    [Switch]
+    $ReadOnly,
+    
+    # Write contents to a file, whose path is specified by -output
+    [String]
+    $Output,
+
+    # Override Polling Period
+    [Int]
+    $PollingFrequency = 60
+)
+
+# If no path was set, prompt for one. If ReadOnly was specified, output a message indicating this is the case
+if(!$ReadOnly) {
+    if($Output -eq $NULL -Or $Output -eq ""){
+        $Output = Read-Host -Prompt "Input a filename"
+
+        if($Output -eq "") {
+            # Since no path was specified, assuming ReadOnly
+            $ReadOnly = $TRUE;
+            Write-Host "Warning! No output path was specified, assuming ReadOnly mode." -ForegroundColor Red
+        }
+    }
+} else {
+    Write-Host "ReadOnly Mode Active! No values are being recorded." -ForegroundColor Green
+}
+
+# Main Loop
 while(1) {
-      # If the path does not exist, start by adding the header row
-      if(-Not (Test-Path $path)) {
-            Add-Content $path "FullChargeCapacity,DesignedCapacity,EstimatedRemainingCapacity,EstimatedChargeRemaining,Timestamp"
+      if(!$ReadOnly){
+          # If the path does not exist, start by adding the header row
+          if(-Not (Test-Path $Output)) {
+              Add-Content $Output "DesignedCapacity,FullChargeCapacity,EstimatedRemainingCapacity,EstimatedChargeRemaining,Timestamp"
+          }
       }
 
       $strComputer = "LocalHost" 
 
       # Get the full charge capacity and write it out to the console
       $fullChargeCapacity = (Get-WmiObject -Class "BatteryFullChargedCapacity" -Namespace "ROOT\WMI").FullChargedCapacity
-      write-host "Full Charged Capacity: " $fullChargeCapacity
 
       # Get the designed capacity of the battery and write it out the console
       $designedCapacity = (Get-WmiObject -Class "BatteryStaticData" -Namespace "ROOT\WMI").DesignedCapacity
-      write-host "Designed Capacity: " $designedCapacity
+
+      # Get On Battery / AC Status
+      $onPowerLine = (Get-WmiObject -class "BatteryStatus" -namespace "ROOT\WMI").PowerOnLine
 
       # Get all objects as reported by the Win32_Battery class
-      $colItems = get-wmiobject -class "Win32_Battery" -namespace "root\CIMV2" -computername $strComputer 
+      $colItems = Get-WmiObject -Class "Win32_Battery" -Namespace "ROOT\CIMV2" -ComputerName $strComputer
 
       # Get the current Timestamp
       $ts = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
       # Generate the Record to output to the file
-      $outputLine = "$fullChargeCapacity,$designedCapacity,$($colItems.EstimatedChargeRemaining / 100 * $designedCapacity),$($colItems.EstimatedChargeRemaining),$ts"
-      Add-Content $path $outputLine
+      $outputLine = "$designedCapacity,$fullChargeCapacity,$($colItems.EstimatedChargeRemaining / 100 * $designedCapacity),$($colItems.EstimatedChargeRemaining),$ts"
+      if(!$ReadOnly) {
+          Add-Content $Output $outputLine
+      }
 
       # Write out the recorded information to the console
-      write-host $outputLine
+      $outputRecord = new-object psobject
+      $outputRecord | add-member -NotePropertyName "Designed Capacity" -NotePropertyValue $designedCapacity
+      $outputRecord | add-member -NotePropertyName "Full Charge Capacity" -NotePropertyValue $fullChargeCapacity
+      $outputRecord | add-member -NotePropertyName "Remaining Capacity" -NotePropertyValue $($colItems.EstimatedChargeRemaining / 100 * $designedCapacity)
+      $outputRecord | add-member -NotePropertyName "Remaining Charge" -NotePropertyValue $($colItems.EstimatedChargeRemaining)
+      $outputRecord | add-member -NotePropertyName "Battery Status" -NotePropertyValue $( If ($onPowerLine -eq $TRUE ) { "AC" } else { "Battery"} )
+      $outputRecord | add-member -NotePropertyName "Time" -NotePropertyValue $ts
+      if($outputRecordHeaders -eq $NULL) {
+          ($outputRecord | ft | Out-String).TrimEnd()
+          $outputRecordHeaders = $TRUE
+      } else {
+          ($outputRecord | ft -HideTableHeaders | Out-String).Replace("`r`n", '')
+      }
+
 
       # Wait 60 Seconds before the next battery poll
-      Start-Sleep -Seconds 60 
+      Start-Sleep -Seconds $PollingFrequency
 }
